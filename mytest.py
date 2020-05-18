3d6faf6db5c9b3232f29c357584b81bca22ce6fc
prakash1602 <65222195+prakash1602@users.noreply.github.com>
2020-05-18 14:08:04 +0530

    Initial commit

---
+++ mytest.py
@@ -0,0 +1,24 @@
+class Test:
+    def __init__(self,balance):
+        self.balance=balance
+    def withdraw(self,amount):
+        if amount<=self.balance:
+            print('Collect Cash')
+            self.balance-=amount
+    def deposit(self,amount):
+        self.balance+=amount
+        print('Deposited')
+
+class Bank:
+    def m1():
+     bal=int(input('Enter your balance:'))
+     acc=Test(bal)
+     print('Your balance is',acc.balance)
+     amt=int(input('Enter your  withdraw amount:'))
+     acc.withdraw(amt)
+     print('Your rest balance is',acc.balance)
+     amt=int(input('Enter your deposit amount:'))
+     acc.deposit(amt)
+     print('Your updated balance is',acc.balance)
+Bank.m1()
+
